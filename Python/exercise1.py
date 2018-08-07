333dbf1168ceff24ecd1a6f388fcc97694591a18
Guilherme Soster <GSoster@users.noreply.github.com>
2018-08-07 18:09:56 -0300

    Python: added some more examples to exercise1

--- Python/exercise1.py
+++ Python/exercise1.py
@@ -38,3 +38,20 @@ b = temp
 
 #OBS: the code below can also be used to swap values, it uses tuples:
 # a, b = b, a
+
+
+####################################################################
+######################## E X E R C I S E 4 #########################
+####################################################################
+
+
+# Variables representing the number of candies collected by alice, bob, and carol
+alice_candies = 121
+bob_candies = 77
+carol_candies = 109
+
+# Your code goes here! Replace the right-hand side of this assignment with an expression
+# involving alice_candies, bob_candies, and carol_candies
+to_smash = (alice_candies + bob_candies + carol_candies) % 3 # == 1
+
+#q4.check()
