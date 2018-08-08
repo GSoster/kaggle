4f60e89afe42a46e4d0e0abd6e45f57812b8bad5
Guilherme Soster <GSoster@users.noreply.github.com>
2018-08-08 17:35:37 -0300

    Python: starting exercises 2

---
+++ Python/exercise2.py
@@ -0,0 +1,20 @@
+####################################################################
+######################## E X E R C I S E 1 #########################
+####################################################################
+def round_to_two_places(num):
+    """Return the given number rounded to two decimal places. 
+    
+    >>> round_to_two_places(3.14159)
+    3.14
+    """
+    # Replace this body with your own code.
+    # ("pass" is a keyword that does literally nothing. We used it as a placeholder
+    # because after we begin a code block, Python requires at least one line of code)
+    #pass
+    return round(num, 2)
+
+#q1.check()
+
+####################################################################
+######################## E X E R C I S E 2 #########################
+####################################################################
