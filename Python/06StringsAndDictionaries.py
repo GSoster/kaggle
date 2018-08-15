3fe9eb23cc02c000bdab862d8a1285539902a6fb
Guilherme Soster <GSoster@users.noreply.github.com>
2018-08-15 16:35:24 -0300

    Python: starting strings and dictionaries

---
+++ Python/06StringsAndDictionaries.py
@@ -0,0 +1,11 @@
+####################################################################
+######################## E X E R C I S E 1 #########################
+####################################################################
+
+def is_valid_zip(zip_code):
+    """Returns whether the input string is a valid (5 digit) zip code
+    """    
+    return (len(zip_code) == 5) and (zip_code.isdigit() == True)
+
+q1.check()
+
